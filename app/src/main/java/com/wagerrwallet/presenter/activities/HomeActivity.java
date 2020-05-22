99495780190367b349e22cce7a8efcc6c4e0469e
MIPPL <mip.putalocura@gmail.com>
2020-05-22 09:18:25 +0200

    remove instaswap in testnet build

--- app/src/main/java/com/wagerrwallet/presenter/activities/HomeActivity.java
+++ app/src/main/java/com/wagerrwallet/presenter/activities/HomeActivity.java
@@ -134,6 +134,7 @@ public class HomeActivity extends BRActivity implements InternetManager.Connecti
             }
         });
 
+        mSwaps.setVisibility( (BuildConfig.BITCOIN_TESTNET) ? View.GONE : View.VISIBLE);
         mSwaps.setBackground( getResources().getDrawable( (BuildConfig.BITCOIN_TESTNET) ? R.drawable.event_menu_shape_testnet : R.drawable.event_menu_shape, null) );
         mSwaps.setOnClickListener(new View.OnClickListener() {
             @Override
